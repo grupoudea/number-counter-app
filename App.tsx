94de085f0d5336ad73e9ddb9efe734476a61d95d
berracode <juanguillermoalarcon@gmail.com>
2023-06-07 23:24:05 -0500

    se usa tamaño de pantalla con hook y con Dimesions

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import CounterScreen from './src/screens/CounterScreen'
 import BoxObjectModel from './src/screens/BoxObjectModel'
 import { SafeAreaView } from 'react-native'
+import DimensionsScreen from './src/screens/DimensionsScreen'
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <BoxObjectModel />
+      <DimensionsScreen />
     </SafeAreaView>
   )
 }
