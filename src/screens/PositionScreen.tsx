e04212c53f18a147a2f8b3bf85be69d1c18afeb1
berracode <juanguillermoalarcon@gmail.com>
2023-06-08 20:16:10 -0500

    se realizan ejercicios con flex

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -9,8 +9,16 @@ const PositionScreen = () => {
             <Text style={styles.title}>B</Text>
             <Text style={styles.title}>C</Text>
         </View>
-        <View style={styles.cajaNaranja}></View>
-        <View style={styles.cajaVerde}></View>
+        <View style={styles.cajaNaranja}>
+            <Text style={styles.title}>A2</Text>
+            <Text style={styles.title}>B2</Text>
+            <Text style={styles.title}>C2</Text>
+        </View>
+        <View style={styles.cajaVerde}>
+            <Text style={styles.title}>A3</Text>
+            <Text style={styles.title}>B3</Text>
+            <Text style={styles.title}>C3</Text>
+        </View>
 
     </View>
   )
@@ -21,30 +29,44 @@ const styles = StyleSheet.create({
         flex: 1,
         height: 500,
         backgroundColor: '#28c4d9',
-        justifyContent: 'space-evenly',
-        //alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
     },
     cajaRoja: {
         width: '100%',
         height: 100,
         backgroundColor: 'red',
         borderWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     cajaNaranja: {
         width:  '100%',
         height: 100,
         backgroundColor: 'orange',
         borderWidth: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center'
     },
     cajaVerde: {
         width:  '100%',
         height: 100,
         backgroundColor: 'green',
         borderWidth: 1,
+        flexDirection: 'row-reverse',
+        justifyContent: 'space-around',
+        alignItems: 'center'
     },
     title: {
+        width: 25,
+        height: 25,
+        backgroundColor: '#d4d4d4',
+        borderWidth: 1,
         fontWeight: 'bold',
-        fontSize: 10,
+        fontSize: 20,
+        color: 'black',
     }
     
 });
