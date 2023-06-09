434f228813a45273ea74f8990da4d1416b0926f4
berracode <juanguillermoalarcon@gmail.com>
2023-06-08 20:45:42 -0500

    counter-screen

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <DimensionsScreen /> */}
-      <PositionScreen />
+      <CounterScreen />
     </SafeAreaView>
   )
 }
